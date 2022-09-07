093286d8f11e4d049bdedeaddd407881033a4e64
alfina.megasiwi <alfina.megasiwi@ui.ac.id>
2022-09-07 09:26:38 +0700

    intial commit 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dashboard Monitoring FE
+          Dashboard Monitoring Front End
         </p>
         <a
           className="App-link"
